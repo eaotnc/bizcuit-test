63a9c4deb86a26511dca41bcebad07fbdfeeeb7d
eao.tnc <thanatcha.eaoe@gmaile.ecom>
2021-10-08 03:11:12 +0700

    feat: add beer card

---
+++ question3/src/BeerCard/BeerCard.tsx
@@ -0,0 +1,10 @@
+
+interface BeerCardProps {
+
+}
+const BeerCard = (props: BeerCardProps): React.ReactElement => {
+    console.log('props', props)
+    return (<div> eao</div>);
+}
+
+export default BeerCard;
