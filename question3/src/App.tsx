33addc409ca17c029da01fa8b17354a7ddbd43fb
eao.tnc <thanatcha.eaoe@gmaile.ecom>
2021-10-08 05:23:10 +0700

    feat: create toggle theme mode

--- question3/src/App.tsx
+++ question3/src/App.tsx
@@ -26,6 +26,7 @@ function App() {
   const [currentDisplay, setCurrentDisplay] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [errorMessage, seterrorMessage] = useState<string>('');
+  const [appTheme, setAppTheme] = useState<string>('dark-mode');
   useEffect((): any => {
     getRandomBeer()
   }, []);
@@ -63,13 +64,33 @@ function App() {
     } finally {
       setLoading(false)
     }
+  }
+  const toggleThemeMode = () => {
+    console.log("🚀 ~ toggleThemeMode ~ appTheme", appTheme)
+
+    if (appTheme === 'dark-mode') {
+      setAppTheme('light-mode')
+    } else {
+      setAppTheme('dark-mode')
+    }
 
   }
   return (
-    <div className="App">
+    <div className={`App ${appTheme}`}>
       <header className="App-header">
         Random Beers
       </header>
+
+
+      <section >
+        <label htmlFor="toggle-2" className="toggle-2">
+          <input type="checkbox" name="toggle-2" id="toggle-2" className="toggle-2__input" onClick={() => toggleThemeMode()} />
+          <span className="toggle-2__button">
+            <img src="https://raw.githubusercontent.com/nueymoo/toggle-switch-css/master/sun.png" alt="sun" className="toggle-2__button--icon" />
+            <img src="https://raw.githubusercontent.com/nueymoo/toggle-switch-css/master/moon.png" alt="moon" className="toggle-2__button--icon" />
+          </span>
+        </label>
+      </section>
 
       {errorMessage &&
         <header className="App-header">
