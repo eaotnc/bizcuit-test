4dfd00b6afcb1f9da1c2367d04bfead95dc54db3
eao.tnc <thanatcha.eaoe@gmaile.ecom>
2021-10-08 03:58:13 +0700

    feat: make display beercard

--- question3/src/App.tsx
+++ question3/src/App.tsx
@@ -1,14 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 import BeerCard from './BeerCard';
+import { useState } from 'react';
+interface BeerDetailInterface {
+  _id: string;
+  uid: string;
+  brand: string;
+  name: string;
+  style: string;
+  hop: string;
+  yeast: string;
+  malts: string;
+  ibu: string;
+  alcohol: string;
+  blg: string;
+  randomCount: number;
+}
 
 function App() {
+  const [beers, setbeers] = useState<Array<BeerDetailInterface>>([{
+    _id: "802",
+    uid: "e491d0a0-d27e-4afc-af1b-41685c65c04e",
+    brand: "Tsingtao",
+    name: "Oaked Arrogant Bastard Ale",
+    style: "Smoke-flavored",
+    hop: "Columbus",
+    yeast: "2035 - American Lager",
+    malts: "Roasted barley",
+    ibu: "21 IBU",
+    alcohol: "3.4%",
+    blg: "19.8°Blg",
+    randomCount: 3,
+  }
+  ]);
+
+  const renderBeer = () => {
+    return beers.map((beer, index) => {
+      return <BeerCard beerDetail={beer} />
+    })
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         Random Beers
       </header>
-      <BeerCard />
+      {
+        renderBeer()
+      }
+
     </div>
   );
 }
--- question3/src/BeerCard/BeerCard.tsx
+++ question3/src/BeerCard/BeerCard.tsx
@@ -1,10 +1,77 @@
-
-interface BeerCardProps {
-
+import './BeerCard.css'
+interface BeerDetailInterface {
+    _id: string;
+    uid: string;
+    brand: string;
+    name: string;
+    style: string;
+    hop: string;
+    yeast: string;
+    malts: string;
+    ibu: string;
+    alcohol: string;
+    blg: string;
+    randomCount: number;
 }
-const BeerCard = (props: BeerCardProps): React.ReactElement => {
-    console.log('props', props)
-    return (<div> eao</div>);
+
+const BeerCard = (props: {
+    beerDetail: BeerDetailInterface
+}):
+    React.ReactElement => {
+    return (
+        <div className="beer-card">
+            <div>
+                <div className="beer-row">
+                    <div className="beer-header">_id : </div>
+                    <div className="beer-info">{props.beerDetail._id}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">uid : </div>
+                    <div className="beer-info">{props.beerDetail.uid}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">brand : </div>
+                    <div className="beer-info">{props.beerDetail.brand}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">name : </div>
+                    <div className="beer-info">{props.beerDetail.name}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">style : </div>
+                    <div className="beer-info">{props.beerDetail.style}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">hop : </div>
+                    <div className="beer-info">{props.beerDetail.hop}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">yeast : </div>
+                    <div className="beer-info">{props.beerDetail.yeast}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">malts : </div>
+                    <div className="beer-info">{props.beerDetail.malts}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">ibu : </div>
+                    <div className="beer-info">{props.beerDetail.ibu}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">alcohol : </div>
+                    <div className="beer-info">{props.beerDetail.alcohol}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">blg : </div>
+                    <div className="beer-info">{props.beerDetail.blg}</div>
+                </div>
+                <div className="beer-row">
+                    <div className="beer-header">randomCount : </div>
+                    <div className="beer-info">{props.beerDetail.randomCount}</div>
+                </div>
+
+            </div>
+        </div>);
 }
 
 export default BeerCard;
