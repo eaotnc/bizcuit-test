41b2a19a62131420ea35ab549a6bab7af438677e
eao.tnc <thanatcha.eaoe@gmaile.ecom>
2021-10-08 04:07:57 +0700

    feat: add cors at backend

--- question2/src/app.js
+++ question2/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const BodyParser = require("body-parser");
 const BeerRoutes = require("./routes/BeerRoutes");
 
+app.use(cors());
 // parse application/json
 app.use(BodyParser.json());
 
